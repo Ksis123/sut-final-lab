9be336a6f9f97187fdd6532109c16710191f60a0
Ksis123 <khemkhemsiwa555@gmail.com>
2023-02-15 14:34:28 -0800

    b6311223_negative_employee - close #4

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -6,5 +6,5 @@ type Employee struct {
 	gorm.Model
 	Name       string `valid:"required~Name cannot be null"`
 	Email      string
-	EmployeeID string
+	EmployeeID string `valid:"matches(^([J|M|S])([0-9]{8})$)~EmployeeID must be JMS_12345678"`
 }
